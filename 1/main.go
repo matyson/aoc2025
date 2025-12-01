1d035cd3de9664c788eb416c0e1337d9718014fd
Matheus Bernardi <matheus_luis.bernardi@maxiv.lu.se>
2025-12-01 23:06:06 +0100

    d1p1

--- 1/main.go
+++ 1/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	args := os.Args[1:]
+	filename := args[0]
+
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("failed to open file: %s", err)
+	}
+	defer f.Close()
+
+	pass := 0
+	dial := 50
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		direction := string(line[0])
+		steps, err := strconv.Atoi(line[1:])
+		if err != nil {
+			log.Fatalf("failed to convert steps to int: %s", err)
+		}
+
+		switch direction {
+		case "L":
+			dial = (dial - steps + 100) % 100
+		case "R":
+			dial = (dial + steps) % 100
+		}
+
+		if dial == 0 {
+			pass++
+		}
+
+	}
+	fmt.Println("the password is:", pass)
+
 }
