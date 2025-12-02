39c59412ce3fae70801684d1fa60a696bfc0c7c5
Matheus Bernardi <matheus_luis.bernardi@maxiv.lu.se>
2025-12-02 12:15:59 +0100

    d1p2

--- 1/main.go
+++ 1/main.go
@@ -12,6 +12,12 @@ func main() {
 	args := os.Args[1:]
 	filename := args[0]
 
+	p1(filename)
+	p2(filename)
+
+}
+
+func p1(filename string) {
 	f, err := os.Open(filename)
 	if err != nil {
 		log.Fatalf("failed to open file: %s", err)
@@ -32,7 +38,7 @@ func main() {
 
 		switch direction {
 		case "L":
-			dial = (dial - steps + 100) % 100
+			dial = ((dial-steps)%100 + 100) % 100
 		case "R":
 			dial = (dial + steps) % 100
 		}
@@ -42,6 +48,49 @@ func main() {
 		}
 
 	}
-	fmt.Println("the password is:", pass)
+	fmt.Println("the password was:", pass)
 
+}
+
+func p2(filename string) {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("failed to open file: %s", err)
+	}
+	defer f.Close()
+
+	pass := 0
+	dial := 50
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		direction := string(line[0])
+		steps, err := strconv.Atoi(line[1:])
+		if err != nil {
+			log.Fatalf("failed to convert steps to int: %s", err)
+		}
+		var cycles int
+		switch direction {
+		case "L":
+			if dial == 0 {
+				cycles = steps / 100
+			}
+
+			diff := dial - steps
+			if diff <= 0 && dial > 0 {
+				cycles = -diff/100 + 1
+			}
+
+			dial = ((dial-steps)%100 + 100) % 100
+
+		case "R":
+			cycles = (dial + steps) / 100
+			dial = (dial + steps) % 100
+		}
+
+		pass += cycles
+
+	}
+	fmt.Println("the password is:", pass)
 }
