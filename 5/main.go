a5aeb5966abe87490401b7437331a8deedebd6e6
Matheus Bernardi <matheus_luis.bernardi@maxiv.lu.se>
2025-12-06 17:59:11 +0100

    d6p2

--- 5/main.go
+++ 5/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -13,6 +14,7 @@ func main() {
 	filename := args[0]
 
 	p1(filename)
+	p2(filename)
 }
 
 func parseDB(filename string) ([][2]int, []int) {
@@ -21,23 +23,13 @@ func parseDB(filename string) ([][2]int, []int) {
 		log.Fatalf("failed to read file: %s", err)
 	}
 
-	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
+	lines := strings.Split(strings.TrimSpace(string(data)), "\n\n")
 	freshList := make([][2]int, 0)
 	checkList := make([]int, 0)
 
-	for _, line := range lines {
-		if len(line) == 0 {
-			continue
-		}
+	for _, line := range strings.Split(lines[0], "\n") {
 		parts := strings.Split(line, "-")
-		if len(parts) != 2 {
-			id, err := strconv.Atoi(parts[0])
-			if err != nil {
-				log.Fatalf("failed to parse checklist id: %s", err)
-			}
-			checkList = append(checkList, id)
-			continue
-		}
+
 		start, err := strconv.Atoi(parts[0])
 		if err != nil {
 			log.Fatalf("failed to parse range start: %s", err)
@@ -47,6 +39,14 @@ func parseDB(filename string) ([][2]int, []int) {
 			log.Fatalf("failed to parse range end: %s", err)
 		}
 		freshList = append(freshList, [2]int{start, end})
+	}
+
+	for _, line := range strings.Split(lines[1], "\n") {
+		id, err := strconv.Atoi(strings.TrimSpace(line))
+		if err != nil {
+			log.Fatalf("failed to parse ingredient id: %s", err)
+		}
+		checkList = append(checkList, id)
 	}
 
 	return freshList, checkList
@@ -71,4 +71,40 @@ func p1(filename string) {
 	}
 
 	fmt.Println("fresh ingredients:", freshCount)
+}
+
+func p2(filename string) {
+	freshList, _ := parseDB(filename)
+
+	slices.SortFunc(freshList, func(a, b [2]int) int {
+		if a[0] < b[0] {
+			return -1
+		} else if a[0] > b[0] {
+			return 1
+		}
+		return 0
+	})
+
+	mergedList := make([][2]int, 0)
+	for _, r := range freshList {
+		if len(mergedList) == 0 {
+			mergedList = append(mergedList, r)
+			continue
+		}
+
+		lastRange := mergedList[len(mergedList)-1]
+		if r[0] <= lastRange[1]+1 {
+			if r[1] > lastRange[1] {
+				mergedList[len(mergedList)-1][1] = r[1]
+			}
+		} else {
+			mergedList = append(mergedList, r)
+		}
+	}
+
+	totalFresh := 0
+	for _, r := range mergedList {
+		totalFresh += r[1] - r[0] + 1
+	}
+	fmt.Println("total fresh ingredients:", totalFresh)
 }
